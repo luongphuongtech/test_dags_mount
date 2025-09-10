948ff5626f75ae133b244a4539d70c3ce5f7bc36
Luong Ngoc Phuong <121532605+luongphuongtech@users.noreply.github.com>
2025-09-10 18:09:00 +0700

    Update test2.py

--- test2.py
+++ test2.py
@@ -1,25 +1,23 @@
 from datetime import datetime
 from airflow import DAG
-from trino_oauth2_operator import TrinoOAuth2Operator
-from airflow import DAG
-from airflow.operators.python import PythonOperator
-from datetime import datetime, timedelta
+from airflow.providers.trino.operators.trino import TrinoOperator
 
-dag = DAG(
-    'trino_oauth2_test_dag',
-    description='Test Trino OAuth2 Hook và Operator',
-    schedule=timedelta(days=1),
-    start_date=datetime(2023, 9, 10),
+# Định nghĩa DAG
+with DAG(
+    dag_id="example_trino_operator",
+    schedule_interval="@daily",   # chạy hàng ngày
+    start_date=datetime(2023, 1, 1),
     catchup=False,
-)
+    tags=["trino", "example"],
+) as dag:
 
-sql_query = "SHOW CATALOGS"
+    # Task chạy query trên Trino
+    run_trino_query = TrinoOperator(
+        task_id="run_trino_query",
+        trino_conn_id="trino_default_oauth2",  # connection ID cấu hình trong Airflow
+        sql="""
+            show catalogs;
+        """,
+    )
 
-run_trino_sql =TrinoOAuth2Operator(
-    task_id='run_trino_sql_task',
-    sql=sql_query,
-    trino_conn_id='trino_default_oauth2',  # Sử dụng kết nối Trino đã được tạo trong Airflow UI
-    dag=dag
-)
-
-run_trino_sql
+    run_trino_query
