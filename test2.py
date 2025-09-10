6621be873997e4199c50d9d944e2ce8e682eca71
Luong Ngoc Phuong <121532605+luongphuongtech@users.noreply.github.com>
2025-09-11 00:42:19 +0700

    Update test2.py

--- test2.py
+++ test2.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 from airflow import DAG
-from airflow.providers.amazon.aws.operators.s3 import S3CreateBucketOperator
+from airflow.providers.trino.hooks.trino import TrinoHook
 
 # Định nghĩa DAG
 with DAG(
