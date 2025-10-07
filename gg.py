118a1c924911f8b1b122d1562569b2bcd771f57a
Luong Ngoc Phuong <121532605+luongphuongtech@users.noreply.github.com>
2025-10-07 16:28:53 +0700

    Create gg.py

---
+++ gg.py
@@ -0,0 +1,20 @@
+from datetime import datetime, timedelta
+from airflow import DAG
+from test_plugin import HelloOperator  # import operator bạn đã tạo
+
+with DAG(
+    dag_id="test_trino_oauth2_show_catalogs",
+    description="DAG test TrinoOAuth2Operator với lệnh SHOW CATALOGS",
+    start_date=datetime(2024, 1, 1),
+    schedule=timedelta(days=1),
+    catchup=False,
+    tags=["trino", "oauth2", "test"]
+) as dag:
+
+    show_catalogs = TrinoOAuth2Operator(
+        task_id="show_trino_catalogs",
+        sql="SHOW CATALOGS",  # câu lệnh Trino
+        trino_conn_id="trino_default_oauth2"
+    )
+
+    show_catalogs
