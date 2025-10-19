db79e141c6198176bc6b6ac4935395bc02cc1d63
Luong Ngoc Phuong <121532605+luongphuongtech@users.noreply.github.com>
2025-10-20 01:12:12 +0700

    Replace TrinoOAuth2Operator with TrinoOperator

--- test_trino.py
+++ test_trino.py
@@ -1,6 +1,6 @@
 from datetime import datetime, timedelta
 from airflow import DAG
-from trino_oauth2_operator import TrinoOAuth2Operator  # import operator bạn đã tạo
+from airflow.providers.trino.operators.trino import TrinoOperator
 
 with DAG(
     dag_id="test_trino_oauth2_show_catalogs",
@@ -10,11 +10,9 @@ with DAG(
     catchup=False,
     tags=["trino", "oauth2", "test"]
 ) as dag:
-
-    show_catalogs = TrinoOAuth2Operator(
+    show_catalogs = TrinoOperator(
         task_id="show_trino_catalogs",
         sql="show schemas from tpch",  # câu lệnh Trino
         trino_conn_id="trino_default_oauth2"
     )
-
     show_catalogs
