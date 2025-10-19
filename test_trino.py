d9d1e933e6f4017e67ef24de7fe9dca896472bb4
Luong Ngoc Phuong <121532605+luongphuongtech@users.noreply.github.com>
2025-10-20 01:39:34 +0700

    Add function to run Trino query in DAG

--- test_trino.py
+++ test_trino.py
@@ -2,6 +2,15 @@ from datetime import datetime, timedelta
 from airflow import DAG
 from airflow.providers.trino.hooks.trino import TrinoHook
 
+def run_trino_query(**context):
+    """Hàm chạy SQL query bằng TrinoHook"""
+    hook = TrinoHook(trino_conn_id="trino_default_oauth2")
+    sql = "SHOW CATALOGS"
+    records = hook.get_records(sql)
+    for row in records:
+        print(row)
+    return records
+
 with DAG(
     dag_id="test_trino_oauth2_show_catalogs",
     description="DAG test TrinoOAuth2Operator với lệnh SHOW CATALOGS",
@@ -10,9 +19,9 @@ with DAG(
     catchup=False,
     tags=["trino", "oauth2", "test"]
 ) as dag:
-    show_catalogs = TrinoOperator(
-        task_id="show_trino_catalogs",
-        sql="show schemas from tpch",  # câu lệnh Trino
-        trino_conn_id="trino_default_oauth2"
+    show_catalogs = PythonOperator(
+        task_id="run_show_catalogs",
+        python_callable=run_trino_query,
+        provide_context=True,
     )
     show_catalogs
